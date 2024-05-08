fd447fedd77f05cb0723ae39fc0d58528a722618
a886866 <a886866@DESKTOP-FB9EEHT>
2024-05-08 16:45:10 +0530

    added middleware  validator for email and passwrod request to

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const UserController = require("../../controllers/user-contoller");
 const router = express.Router();
-router.post("/signup", UserController.create);
-router.post("/signin", UserController.signin);
+const { AuthRequestValidator } = require("../../middlewares/index");
+router.post(
+  "/signup",
+  AuthRequestValidator.validateUserAuth,
+  UserController.create
+);
+router.post(
+  "/signin",
+  AuthRequestValidator.validateUserAuth,
+  UserController.signin
+);
 module.exports = router;
